8f035c1621d26560e96c79d7762fd19d870544ed
chikunmark <chikunmark@gmail.com>
2023-02-24 15:31:41 +0800

    feat: build index page & detail page

--- app.js
+++ app.js
@@ -4,13 +4,32 @@ const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
 const app = express()
 const PORT = 3000
+
+const db = require('./models')
+const Todo = db.Todo
+const User = db.User
+
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
-  res.send('hello world')
+  return Todo.findAll({ raw: true, nest: true })
+    .then(todos => {
+      console.log(todos)
+      return res.render('index', { todos: todos })
+    })
+    .catch(error => {
+      return res.status(422).json(error)
+    })
+})
+
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.findByPk(id)
+    .then(todo => res.render('detail', { todo: todo.toJSON() }))
+    .catch(error => console.log(error))
 })
 
 app.get('/users/login', (req, res) => {
@@ -26,7 +45,8 @@ app.get('/users/register', (req, res) => {
 })
 
 app.post('/users/register', (req, res) => {
-  res.send('register')
+  const { name, email, password, confirmPassword } = req.body
+  User.create({ name, email, password }).then(user => res.redirect('/'))
 })
 
 app.get('/users/logout', (req, res) => {
